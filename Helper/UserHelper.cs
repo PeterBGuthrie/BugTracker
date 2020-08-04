3a55b3db78c5ca9b5fc0e590c4713733f1e0a188
Peter Guthrie <peterbguthrie@gmail.com>
2020-08-04 16:00:12 -0400

    At 1:42 morning lecture evening lecture about to start

---
+++ Helper/UserHelper.cs
@@ -0,0 +1,27 @@
+﻿using BugTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BugTracker.Helper
+{
+    public class UserHelper
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public string GetFullName(string userId)
+        {
+            var firstName = db.Users.Find(userId).FirstName;
+            var LastName = db.Users.Find(userId).LastName;
+            var fullName = firstName + " " + LastName;
+            return fullName;
+        }
+
+        public string LastNameFirst(string userId)
+        {
+            var user = db.Users.Find(userId);
+            return user.FullName;
+        }
+    }
+}
